86bd5b4497ff226536a2ca4ecd7c7cc03d3904a6
Afreen Rahman <afreen.rahman25@gmail.com>
2025-10-23 14:10:43 -0700

    sol 11

---
+++ PythonSolutions/q11.py
@@ -0,0 +1,28 @@
+# Container with Most Water - LeetCode 11
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        """
+        This function calculates the maximum area that can be formed from a list of lines of different heights.
+        The lines are represented as a list of integers where each integer is the height of the line.
+        The function takes the list of heights as input and returns the maximum area that can be formed.
+        """
+        maxVol = 0
+        l,r = 0, len(height) - 1
+
+        # loop until we have processed all the lines
+        while l < r:
+            # calculate the current volume
+            currVol = min(height[l], height[r]) * (r-l)
+            # update the maximum volume if the current volume is greater
+            maxVol = max(currVol,maxVol)
+            # if the left line is shorter, move the left pointer to the right
+            if height[l] < height[r]:
+                l+=1
+            # else move the right pointer to the left
+            else:
+                r-=1
+        return maxVol
+
+        # Time complexity: O(n)
+        # Space complexity: O(1)
