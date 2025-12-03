d14fb362af1a2c0a4494f5f2eaee2854782b498e
Afreen Rahman <afreen.rahman25@gmail.com>
2025-12-02 23:01:51 -0800

    Solutions

---
+++ PythonSolutions/q238.py
@@ -0,0 +1,32 @@
+# 238. Product of Array Except Self
+# https://leetcode.com/problems/product-of-array-except-self/
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """
+        This function calculates the product of all elements in a list except for the element at the current index.
+        It uses two pointers, one moving from left to right and the other moving from right to left.
+        The product of all elements to the left of the current index is stored in the left variable.
+        The product of all elements to the right of the current index is stored in the right variable.
+        The product of the two variables is the product of all elements in the list except for the element at the current index.
+        :param nums: A list of integers
+        :return: A list of integers where each element is the product of all elements in the input list except for the element at the same index
+        :rtype: List[int]
+        """
+        sol = [1] * len(nums)
+        n = len(nums)
+
+        left = 1
+        for i in range(n):
+            sol[i] *= left
+            left *= nums[i]
+
+        right = 1
+        for i in range(n-1,-1,-1):
+            sol[i] *= right
+            right *= nums[i]
+            
+        return sol
+
+# Time complexity: O(n)
+# Space complexity: O(1) (excluding the output list)
